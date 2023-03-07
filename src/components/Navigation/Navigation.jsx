c22dba7d554a53c8d05ca7da35d93b92f32e9723
Ruben Briongos <ruben.briongos@gmail.com>
2023-03-07 13:44:33 +0100

    navigation bar auth context

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,17 @@
+import { useContext } from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../contexts/auth.context'
+// import { ThemeContext } from '../../contexts/theme.context'
+
 
 const Navigation = () => {
+
+    // const { themeValue, switchTheme } = useContext(ThemeContext)
+    const { user, logout } = useContext(AuthContext)
+
+    // const navbarStyle = themeValue === 'dark' ? 'light' : 'dark'
+    // const themeText = themeValue === 'light' ? 'Tema oscuro' : 'Tema claro'
 
     return (
         <Navbar bg="white" variant="light" expand="lg" className='navBar'>  {/* bg white is not a thing */}
@@ -16,19 +26,41 @@ const Navigation = () => {
                         <Link to="/battles">
                             <Nav.Link as="span">Battles</Nav.Link>
                         </Link>
-                        <Link to="/battles/create">
-                            <Nav.Link as="span">Create Battle</Nav.Link>
-                        </Link>
-                        <Link to="/signup">
-                            <Nav.Link as="span">Sign Up</Nav.Link>
-                        </Link>
-                        <Link to="/login">
-                            <Nav.Link as="span">Log In</Nav.Link>
-                        </Link>
+                        {
+                            user
+                                ?
+                                <>
+                                    <Link to="/battles/create">
+                                        <Nav.Link as="span">Create Battle</Nav.Link>
+                                    </Link>
+                                    <Link>
+                                        <Nav.Link as="span" onClick={logout}>Log Out</Nav.Link>
+                                    </Link>
+                                </>
+                                :
+                                <>
+                                    <Link to="/login">
+                                        <Nav.Link as="span">Log In</Nav.Link>
+                                    </Link>
+                                    <Link to="/signup">
+                                        <Nav.Link as="span">Sign Up</Nav.Link>
+                                    </Link>
+                                </>
+                        }
                     </Nav>
+                    {
+                        user
+                        &&
+                        <Link to={`/details/${user._id}`}>
+                            <Nav.Link as="span">{user.username}, you are the best!</Nav.Link>
+                        </Link>
+                    }
+                    {/* <Navbar.Text>
+                        <span onClick={switchTheme} className="d-flex">{themeText}</span>
+                    </Navbar.Text> */}
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     )
 }
 
