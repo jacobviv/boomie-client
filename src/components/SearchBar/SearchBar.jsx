fe4307e055c726e9a63eaff451d9d56bcf500e76
Ruben Briongos <ruben.briongos@gmail.com>
2023-03-08 10:48:38 +0100

    remove .env from tracking

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Form } from "react-bootstrap"
-import { useContext } from "react"
+// import { useContext } from "react"
 // import { ThemeContext } from "../../contexts/theme.context"
 
 const SearchBar = ({ handleSearchBar }) => {
